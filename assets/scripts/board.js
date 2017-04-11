0c157ee464cb38309b15ae155786c4a017ae46c9
nriser <riserika@gmail.com>
2017-04-11 10:45:41 -0400

    Create arrays in board.js
    Store each box button in a variable. Create empty array to store
    inputted Xs and Os.

---
+++ assets/scripts/board.js
@@ -0,0 +1,23 @@
+// Array of X and O texts
+let xoSymbols = ['\u2613','\u26AC']
+// reference; https://www.w3schools.com/charsets/tryit.asp?deci=9900 ; https://www.w3schools.com/charsets/ref_utf_symbols.asp
+// console.log(playerInput);
+
+// Empty array to store inputted Xs and Os
+let playerInput = []
+
+// Empty array to store each button/box
+let boxArray = []
+// Store each button/box into array
+for (let i = 0; i < 9; i++) { // box0 ~ box8
+  boxArray[i] = document.getElementById('box' + i)
+}
+
+
+
+
+
+
+module.exports = {
+
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,6 +17,9 @@ const authEvents = require('./auth/events.js')
 // use require without a reference to ensure a file is bundled
 
 
+require('./board.js')
+
+
 $(() => {
   authEvents.addHandlers()
 })
