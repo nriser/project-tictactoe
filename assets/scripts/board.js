8996e2003d08a47251acea61a8e60cfaf012f593
nriser <riserika@gmail.com>
2017-04-12 23:35:27 -0400

    Updated 'Update game' feature
    Updates game object's cell index and value info in console whenever any
    box on the board is clicked. Cell index and value correspond to the
    index and value of the clicked box.

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // document.getElementById('board').style.visibility = 'hidden'
 
 // To Do:
@@ -23,19 +22,20 @@ for (let i = 0; i < 9; i++) { // box0 ~ box8
 }
 
 // run function when any box clicked
-const displayInput = function () {
+const displayInput = function (lastClickedBox) {
   // "this" refers to each box in boxArray (boxArray[i])
   // Display player X/O input on clicked box. Start with X.
-  this.textContent = xoSymbols[0]
+  boxArray[lastClickedBox].textContent = xoSymbols[0]
   // push user's X/O input into playerInput array
-  playerInput.push(this.textContent)
+  playerInput.push(boxArray[lastClickedBox].textContent)
   // Disable button once user clicks on a box
-  this.disabled = true
+  boxArray[lastClickedBox].disabled = true
   // run function that display win/draw message
   displayMessage()
+  // update progress of tictactoe game
+  updateCurrentGame(lastClickedBox, xoSymbols[0])
   // reverse X/O input
   xoSymbols.reverse()
-  updateCurrentGame(2, 'o')
 }
 
 const api = require('./game/api.js')
@@ -51,7 +51,9 @@ const updateCurrentGame = function (index, value) {
 // Add click event to each box
 for (let i = 0; i < 9; i++) {
   // boxArray[i].addEventListener("click", displayInput);
-  $(boxArray[i]).on('click', displayInput)
+  $(boxArray[i]).on('click', function () {
+    displayInput(i)
+  })
 }
 
 // Note: addEventListner can be used only on ONE element at a time
